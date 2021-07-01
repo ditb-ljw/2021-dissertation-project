427fbfc3c00163b2beca434f7cc2ef4c9bd880e4
ditb-ljw <s2054084@ed.ac.uk>
2021-07-01 23:05:41 +0100

    Extracted data from the text file

--- data.py
+++ data.py
@@ -1,1 +1,17 @@
 import numpy as np
+
+# read all the contents in the text file
+file = open('CAB.txt','r')
+contents = file.readlines()
+file.close()
+
+# extract the data and save in two matrices
+data = contents[28:652]
+W = np.zeros((25,25))
+C = np.zeros((25,25))
+for i in data:
+    row_content = list(map(float, i.split()))
+    row_i = int(row_content[0]) - 1
+    col_j = int(row_content[1]) - 1
+    W[row_i, col_j] = row_content[2]
+    C[row_i, col_j] = row_content[3]
