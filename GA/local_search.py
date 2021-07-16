21036ddfbbb0af55bfa2e5e45c7240841761b034
ditb-ljw <s2054084@ed.ac.uk>
2021-07-16 23:33:22 +0100

    added a function to generate a random initial matrix for chromosome

--- GA/local_search.py
+++ GA/local_search.py
@@ -1,41 +1,32 @@
 import numpy as np
 import random
-from GA.Chromosomes import chromosome
-from GA.data_processing import test_data
 
 
-def generate_initial_population():
-#    pop_size
-    max_capacity = test_data()[1]
-    demand_dict = test_data()[3]
-    scenarios = test_data()[4]
-    module_capacity = test_data()[6]
+def rand_chromosome_matrix(N_num, hub_locations, highest_originate):
+    '''
+    Generate a random initial capacity(module) matrix for chromosome(may be infeasible).
 
-    population = {}
-    node_number = len(max_capacity)
-    time_periods = len(demand_dict[0])
+    Input:
+        N_num (int): Number of nodes
+        hub_locations (list): Indices of potential locations for the hubs
+        highest_originate (ndarray): Highest total amount of flow originated at each node in each time period
 
-#    time_0_max_demand = max(np.sum(demand_dict[s][0]) for s in scenarios)
-#    total_num_module = np.ceil(time_0_max_demand / module_capacity)
-#    average_module_hub = int(total_num_module / node_number)
+    Output:
+        chromosome_matrix (ndarray): A random initial capacity(module) matrix for chromosome(may be infeasible)
+    '''
 
-    chromosome_matrix = np.zeros([time_periods,node_number])
-    random_chromosome = chromosome(chromosome_matrix)
-    while random_chromosome.if_feasible() == False:
-        chromosome_matrix = np.zeros([time_periods,node_number])
-        old_hubs = []
-        for i in range(time_periods):
-            for j in range(node_number):
-                if j not in old_hubs:
-                    module_number = random.choice(range(max_capacity[j]+1))
-                    if module_number > 0:
-                        chromosome_matrix[i,j] = module_number
-                        old_hubs.append(j)
-                else:
-                    continue
-        random_chromosome = chromosome(chromosome_matrix)
+    chromosome_matrix = np.zeros([3,N_num], dtype=int)
+    old_hubs = []
+    for t in range(3):
+        for k in hub_locations:
+            if k not in old_hubs:
+                build_hub = random.choice([0,1])
+            else:
+                continue
+            if build_hub == 1:
+                at_least_modules = highest_originate[t, k]
+                module_number = random.choice(range(at_least_modules, 6))
+                chromosome_matrix[t,k] = module_number
+                old_hubs.append(k)
 
-    return random_chromosome
-
-
-generate_initial_population().initial_capacity_matrix
+    return chromosome_matrix
