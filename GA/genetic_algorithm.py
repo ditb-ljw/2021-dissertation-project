16dec67115cee8fc8a0a1173dfb4fb152bd18435
ditb-ljw <s2054084@ed.ac.uk>
2021-07-16 22:28:15 +0100

    test case

--- GA/genetic_algorithm.py
+++ GA/genetic_algorithm.py
@@ -1,7 +1,16 @@
 import numpy as np
 from GA.Chromosomes import chromosome
+from GA.data_processing import W, C, CAB_data_processing
 
-initial_capacity_matrix = np.array([[0, 3, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 0, 0]])
-test_c = chromosome(initial_capacity_matrix)
-test_c.if_feasible()
-test_c.fitness()
+
+N_P = [15, 10]
+gamma_alpha = [0.075, 0.2]
+
+distance, coefficients, demand_dict, highest_originate, module_capacity, install_hub_cost_matrix, initial_capacity_cost_dict, additional_capacity_cost_dict = CAB_data_processing(W, C, N_P, gamma_alpha)
+
+# test
+hub_locations = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+initial_capacity_matrix = np.array([[5, 1, 1, 3, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+test_c = chromosome(initial_capacity_matrix, distance, hub_locations, coefficients, demand_dict, highest_originate, module_capacity, install_hub_cost_matrix, initial_capacity_cost_dict, additional_capacity_cost_dict)
+print(test_c.if_feasible())
+print(test_c.fitness())
