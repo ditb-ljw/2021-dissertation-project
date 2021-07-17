52af042d335181c749fa88fbfde055b2d15fabe5
ditb-ljw <s2054084@ed.ac.uk>
2021-07-18 00:58:58 +0100

    Added more tests

--- GA/genetic_algorithm.py
+++ GA/genetic_algorithm.py
@@ -2,7 +2,7 @@ import numpy as np
 from GA.Chromosomes import chromosome
 from GA.data_processing import W, C, CAB_data_processing
 from GA.local_search import rand_chromosome_matrix
-
+from GA.local_search import rand_neighbourhood
 
 N_P = [15, 10]
 gamma_alpha = [0.075, 0.2]
@@ -13,8 +13,13 @@ distance, coefficients, demand_dict, highest_originate, module_capacity, install
 hub_locations = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 initial_capacity_matrix = rand_chromosome_matrix(N_P[0], hub_locations, highest_originate)
+neighbourhood_matrix = rand_neighbourhood(initial_capacity_matrix, hub_locations, highest_originate)
 
 #initial_capacity_matrix = np.array([[5, 1, 1, 3, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
-test_c = chromosome(initial_capacity_matrix, distance, hub_locations, coefficients, demand_dict, highest_originate, module_capacity, install_hub_cost_matrix, initial_capacity_cost_dict, additional_capacity_cost_dict)
-print(test_c.is_feasible())
-print(test_c.fitness())
+test_a = chromosome(initial_capacity_matrix, distance, hub_locations, coefficients, demand_dict, highest_originate, module_capacity, install_hub_cost_matrix, initial_capacity_cost_dict, additional_capacity_cost_dict)
+print(test_a.is_feasible())
+print(test_a.fitness())
+
+test_b = chromosome(neighbourhood_matrix, distance, hub_locations, coefficients, demand_dict, highest_originate, module_capacity, install_hub_cost_matrix, initial_capacity_cost_dict, additional_capacity_cost_dict)
+print(test_b.is_feasible())
+print(test_b.fitness())
