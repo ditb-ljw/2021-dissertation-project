d9fbeef65f59e3de00a79cffda60f6a2db38b511
ditb-ljw <s2054084@ed.ac.uk>
2021-07-18 12:30:28 +0100

    adjusted data structure

--- GA/genetic_algorithm.py
+++ GA/genetic_algorithm.py
@@ -6,20 +6,23 @@ from GA.local_search import rand_neighbourhood
 
 N_P = [15, 10]
 gamma_alpha = [0.075, 0.2]
+hub_locations = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 distance, coefficients, demand_dict, highest_originate, module_capacity, install_hub_cost_matrix, initial_capacity_cost_dict, additional_capacity_cost_dict = CAB_data_processing(W, C, N_P, gamma_alpha)
+test_data = {'distance': distance, 'hub_locations': hub_locations, 'coefficients': coefficients, 'demand_dict': demand_dict, \
+'highest_originate': highest_originate, 'module_capacity': module_capacity, 'install_hub_cost_matrix': install_hub_cost_matrix, \
+'initial_capacity_cost_dict': initial_capacity_cost_dict, 'additional_capacity_cost_dict': additional_capacity_cost_dict}
 
 # test
-hub_locations = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 initial_capacity_matrix = rand_chromosome_matrix(N_P[0], hub_locations, highest_originate)
 neighbourhood_matrix = rand_neighbourhood(initial_capacity_matrix, hub_locations, highest_originate)
 
 #initial_capacity_matrix = np.array([[5, 1, 1, 3, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
-test_a = chromosome(initial_capacity_matrix, distance, hub_locations, coefficients, demand_dict, highest_originate, module_capacity, install_hub_cost_matrix, initial_capacity_cost_dict, additional_capacity_cost_dict)
+test_a = chromosome(initial_capacity_matrix, test_data)
 print(test_a.is_feasible())
 print(test_a.fitness())
 
-test_b = chromosome(neighbourhood_matrix, distance, hub_locations, coefficients, demand_dict, highest_originate, module_capacity, install_hub_cost_matrix, initial_capacity_cost_dict, additional_capacity_cost_dict)
+test_b = chromosome(neighbourhood_matrix, test_data)
 print(test_b.is_feasible())
 print(test_b.fitness())
