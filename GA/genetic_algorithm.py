234f1a1a6bbb3de5272cb6b98df79e1c3d45e0e3
ditb-ljw <s2054084@ed.ac.uk>
2021-07-19 20:11:13 +0100

    added a function to calculate cumulative probability

--- GA/genetic_algorithm.py
+++ GA/genetic_algorithm.py
@@ -1,2 +1,17 @@
-import numpy
+import numpy as np
+import random
 from GA.Chromosomes import chromosome
+
+
+def calculate_cumulative_probability(fitness_list):
+
+    total_sum = sum(fitness_list)
+    norm_fitness_list = [i/total_sum for i in fitness_list]
+    cumulative_probability = np.cumsum(norm_fitness_list)
+
+    return cumulative_probability
+
+
+def roulette_wheel(cumulative_probability):
+
+    rand_num = random.uniform(0, 1)
