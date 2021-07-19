3eaa716c513ee0378ad1e332fa08626a3ea69c77
ditb-ljw <s2054084@ed.ac.uk>
2021-07-19 22:01:22 +0100

    added a function to select a chromosome by roulette wheel selection

--- GA/genetic_algorithm.py
+++ GA/genetic_algorithm.py
@@ -1,9 +1,19 @@
 import numpy as np
 import random
+import bisect
 from GA.Chromosomes import chromosome
 
 
 def calculate_cumulative_probability(fitness_list):
+    '''
+    Calculate the cumulative probability for each chromosome.
+
+    Input:
+        fitness_list (list): Fitness value for each chromosome
+
+    Output:
+        cumulative_probability (list): Cumulative probability for each chromosome
+    '''
 
     total_sum = sum(fitness_list)
     norm_fitness_list = [i/total_sum for i in fitness_list]
@@ -13,5 +23,17 @@ def calculate_cumulative_probability(fitness_list):
 
 
 def roulette_wheel(cumulative_probability):
+    '''
+    Select a chromosome by roulette wheel selection.
+
+    Input:
+        cumulative_probability (list): Cumulative probability for each chromosome
+
+    Output:
+        chromosome_index (int): Index of selected chromosome
+    '''
 
     rand_num = random.uniform(0, 1)
+    chromosome_index = bisect.bisect_left(cumulative_probability, rand_num)
+
+    return chromosome_index
