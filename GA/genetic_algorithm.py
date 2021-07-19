c79a778bc9747a68733b27ec9702d4955a2e26da
ditb-ljw <s2054084@ed.ac.uk>
2021-07-19 22:30:36 +0100

    added a function of 1-point crossover(column)

--- GA/genetic_algorithm.py
+++ GA/genetic_algorithm.py
@@ -37,3 +37,38 @@ def roulette_wheel(cumulative_probability):
     chromosome_index = bisect.bisect_left(cumulative_probability, rand_num)
 
     return chromosome_index
+
+
+def one_pt_col_crossover(chromosome_1, chromosome_2):
+    '''
+    One-point crossover(column): Choose a random point on the two parents, split parents at this crossover point and create children by exchanging tails (columns).
+
+    Input:
+        chromosome_1 (object: chromosome): One of the parent chromosomes
+        chromosome_2 (object: chromosome): One of the parent chromosomes
+
+    Output:
+        child_chromosome_1 (object: chromosome): One of the child chromosomes
+        child_chromosome_2 (object: chromosome): One of the child chromosomes
+    '''
+
+    parent_matrix_1 = chromosome_1.initial_capacity_matrix
+    parent_matrix_2 = chromosome_2.initial_capacity_matrix
+    node_number = parent_matrix_1.shape[1]
+
+    child_matrix_1 = np.zeros([3, node_number], dtype=int)
+    child_matrix_2 = np.zeros([3, node_number], dtype=int)
+
+    # Randomly choose a crossover point
+    split_pt = random.choice(range(1, node_number))
+
+    child_matrix_1[:, :split_pt] = parent_matrix_1[:, :split_pt]
+    child_matrix_1[:, split_pt:] = parent_matrix_2[:, split_pt:]
+    child_matrix_2[:, :split_pt] = parent_matrix_2[:, :split_pt]
+    child_matrix_2[:, split_pt:] = parent_matrix_1[:, split_pt:]
+
+    test_data = chromosome_1.test_data
+    child_chromosome_1 = chromosome(child_matrix_1, test_data)
+    child_chromosome_2 = chromosome(child_matrix_2, test_data)
+
+    return child_chromosome_1, child_chromosome_2
