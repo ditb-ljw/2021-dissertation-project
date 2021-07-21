5bf8a1242808b22885722c2b640a828b973eb32b
ditb-ljw <s2054084@ed.ac.uk>
2021-07-21 20:17:01 +0100

    finished the function for generating a new generation

--- GA/genetic_algorithm.py
+++ GA/genetic_algorithm.py
@@ -231,16 +231,81 @@ def mutation(input_chromosome):
 
 
 
-def new_generation(old_generation):
+def new_generation(old_generation, crossover_probability, mutation_probability):
+    '''
+    Generate new generation by crossover and mutation based on old generation.
 
+    Input:
+        old_generation (list): List of chromosomes of old generation(sorted by their fitness value in decending order)
+        crossover_probability (float): Probability of crossover
+        mutation_probability (float): Probability of mutation
+
+    Output:
+        new_generation (list): List of chromosomes of new generation(sorted by their fitness value in decending order)
+    '''
+
+    population_number = len(old_generation)
     fitness_list = [c.fitness for c in old_generation]
     cumulative_probability = calculate_cumulative_probability(fitness_list)
+    new_generation_list = []
+    new_generation_fitness = []
+    new_generation_matrices = []
 
-    # Select parents
-    parent_index_1 = roulette_wheel(cumulative_probability)
-    parent_index_2 = roulette_wheel(cumulative_probability)
-    while parent_index_2 == parent_index_1:
+    new_generation_length = 0
+    while new_generation_length < population_number:
+        # Select different parents
+        parent_index_1 = roulette_wheel(cumulative_probability)
         parent_index_2 = roulette_wheel(cumulative_probability)
+        while parent_index_2 == parent_index_1:
+            parent_index_2 = roulette_wheel(cumulative_probability)
 
-    parent_chromosome_1 = old_generation[parent_index_1]
-    parent_chromosome_2 = old_generation[parent_index_2]
+        parent_chromosome_1 = old_generation[parent_index_1]
+        parent_chromosome_2 = old_generation[parent_index_2]
+
+        # Crossover
+        rand_num = random.uniform(0, 1)
+        if rand_num <= crossover_probability:
+            child_chromosome_1, child_chromosome_2 = one_pt_col_crossover(parent_chromosome_1, parent_chromosome_2)
+        else:
+            child_chromosome_1, child_chromosome_2 = parent_chromosome_1, parent_chromosome_2
+        # Check
+        for child_chromosome in [child_chromosome_1, child_chromosome_2]:
+            repeating_matrix = False
+            # New chromosomes should be different from each other
+            for j in new_generation_matrices:
+                if (child_chromosome.initial_capacity_matrix == j).all():
+                    repeating_matrix = True
+                    break
+            # Check feasibility
+            if child_chromosome.is_feasible() == True and repeating_matrix == False:
+                # Mutation
+                rand_num = random.uniform(0, 1)
+                if rand_num <= mutation_probability:
+                    child_chromosome = mutation(child_chromosome)
+
+                child_chromosome.calculate_fitness()
+                new_generation_list.append(child_chromosome)
+                new_generation_fitness.append(child_chromosome.fitness)
+                new_generation_matrices.append(child_chromosome.initial_capacity_matrix)
+                new_generation_length += 1
+
+
+    # Elitism
+    best_matrix = old_generation[0].initial_capacity_matrix
+    best_matrix_kept = False
+    for j in new_generation_matrices:
+        if (best_matrix == j).all():
+            best_matrix_kept = True
+            break
+    if best_matrix_kept == False:
+        new_generation_list.append(old_generation[0])
+        new_generation_fitness.append(old_generation[0].fitness)
+        new_generation_matrices.append(best_matrix)
+
+    # Sort in decending order
+    zipped_lists = zip(new_generation_fitness, new_generation_list)
+    sorted_pairs = sorted(zipped_lists, reverse = True)
+    # Choose a fixed number of best chromosomes
+    new_generation = [pair[1] for pair in sorted_pairs[:population_number]]
+
+    return new_generation
