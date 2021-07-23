8e06dc12557d1bfd8e0989f43e47e90ef0b8f39a
ditb-ljw <s2054084@ed.ac.uk>
2021-07-23 20:19:28 +0100

    added constraints

--- GA/full_model.py
+++ GA/full_model.py
@@ -33,7 +33,9 @@ delta = [1, 1, 1]
 # Calculate the rest of data
 N_P = [len(N), len(P)]
 gamma_alpha = [gamma, a]
-distance, demand_dict, originated_flow_dict, destined_flow_dict, install_hub_cost_matrix, initial_f_k_q_t, initial_m_k_q_t, additional_f_k_q_t, additional_m_k_q_t, initial_capacity_cost_dict, additional_capacity_cost_dict = CAB_model_data_processing(W, C, N_P, gamma_alpha)
+distance, demand_dict, originated_flow_dict, destined_flow_dict, \
+install_hub_cost_matrix, initial_f_k_q_t, initial_m_k_q_t, additional_f_k_q_t, additional_m_k_q_t, \
+initial_capacity_cost_dict, additional_capacity_cost_dict = CAB_model_data_processing(W, C, N_P, gamma_alpha)
 
 # flow to be sent from node i to node j in period t in scenario s
 w = demand_dict
@@ -87,3 +89,55 @@ v = M_DE.continuous_var_dict(idx_v, lb=0, name = 'v')
 
 
 # Constraints
+
+# Constraints (4) impose that in each time period and for each potential location for the hubs,
+# if a hub is installed then it must have a certain number of modules which define its initial capacity.
+M_DE.add_constraints((sum(z[(t, k, q)] for q in range(1, Q[k]+1)) == u[(t, k)] \
+                      for k in P for t in T), \
+                      names = '(4)')
+# Constraints (5) state that one hub can be installed at most once during the planning horizon.
+M_DE.add_constraints((sum(u[(t, k)] for t in T) <= 1 \
+                      for k in P), \
+                      names = '(5)')
+# Constraints (43) assure that in each time period and scenario the flow originated in each node is routed via at least one hub.
+M_DE.add_constraints((sum(x[(s, t, i, k)] for k in P) == O[s][t, i] \
+                      for i in N for t in T for s in S), \
+                      names = '(43)')
+# Constraints (44) assure that in each time period and scenario the flow between each pair of nodes is distributed via at least one hub.
+M_DE.add_constraints((sum(v[(s, t, i, l, j)] for l in P) == w[s][t][i, j] \
+                      for i in N for j in N for t in T for s in S), \
+                      names = '(44)')
+# Constraints (45) state that the expansion of the hub’s capacity in a time period and scenario is only allowed if that hub was installed in a previous period.
+M_DE.add_constraints((sum(r[(s, t, k, q)] for q in range(1, Q[k])) <= sum(u[(tau, k)] for tau in range(t)) \
+                      for k in P for t in [1,2] for s in S), \
+                      names = '(45)')
+# Inequalities (46) assure that, for each hub, in each time period and scenario, the incoming flow from non-hubs as well as the flow originated at the hub cannot exceed the hub’s capacity.
+M_DE.add_constraints((sum(x[(s, t, i, k)] for i in N) <= \
+                      gamma*sum(q*z[(tau, k, q)] for q in range(1, Q[k]+1) for tau in range(t+1)) + gamma*sum(q*r[(s, tau, k, q)] for q in range(1, Q[k]) for tau in range(1, t+1)) \
+                      for k in P for t in T for s in S), \
+                      names = '(46)')
+# In each potential location for the hubs there is a maximum number of modules that can be installed as constraints (47) indicate.
+M_DE.add_constraints((sum(q*z[(t, k, q)] for t in T for q in range(1, Q[k]+1)) + sum(q*r[(s, t, k, q)] for t in [1,2] for q in range(1, Q[k])) <= Q[k] \
+                      for k in P for s in S), \
+                      names = '(47)')
+# The flow conservation and divergence constraints are described by (48).
+M_DE.add_constraints((sum(y[(s, t, i, k, l)] for l in P if l != k) - sum(y[(s, t, i, l, k)] for l in P if l != k) == \
+                      x[(s, t, i, k)] - sum(v[(s, t, i, k, j)] for j in N) \
+                      for i in N for k in P for t in T for s in S), \
+                      names = '(48)')
+# The condition defined by (49) ensures that consistent values are obtained for y-variables.
+M_DE.add_constraints((sum(y[(s, t, i, k, l)] for l in P if l != k) <= x[(s, t, i, k)] \
+                      for i in N for k in P for t in T for s in S), \
+                      names = '(49)')
+# Inequalities (50) impose that a hub must collect its own outbound flow.
+M_DE.add_constraints((x[(s, t, i, k)] <= O[s][t, i]*(1 - sum(u[(tau, i)] for tau in range(t+1))) \
+                      for s in S for t in T for i in P for k in P if k != i), \
+                      names = '(50)')
+# Constraints (51) state that the flow destined to any node can be distributed only by an open hub.
+M_DE.add_constraints((sum(v[(s, t, i, l, j)] for i in N) <= D[s][t, j]*sum(u[(tau, l)] for tau in range(t+1)) \
+                      for j in N for l in P for t in T for s in S), \
+                      names = '(51)')
+# A hub needs to distribute its own inbound flow as stated by constraints (52).
+M_DE.add_constraints((v[(s, t, i, l, j)] <= D[s][t, j]*(1 - sum(u[(tau, j)] for tau in range(t+1))) \
+                      for s in S for t in T for i in N for l in P for j in P if j != l), \
+                      names = '(52)')
